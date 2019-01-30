69af7c59d2c23e92f28400f56cea31c1448bb69c
Your Name <you@example.com>
2019-01-30 18:14:40 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Item Variety
+
+App to make item collection, item type, item style
+
+#### License
+
+MIT
---
+++ item_variety/item_variety/doctype/item_type/item_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, hardikgadesha@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Item Type', {
+	refresh: function(frm) {
+
+	}
+});
